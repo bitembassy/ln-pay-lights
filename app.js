6005afe8c811cc7eab9a992c1c84755b28deaa87
aviv57 <aviv57@gmail.com>
2023-04-28 01:44:07 +0300

    Adding skull effect

--- app.js
+++ app.js
@@ -3,6 +3,8 @@ const charge = require('lightning-charge-client')(process.env.CHARGE_URL, proces
 
 const apiKey = process.env.LIFX_KEY
 const apiUrl = `https://api.lifx.com/v1/lights`
+const skullApiUrl = process.env.SKULL_API_URL
+const skullApiPassword = process.env.SKULL_API_PASSWORD
 
 const lifx = (selector, method, action, params) =>
     request[method](`${apiUrl}/${selector}/${action}`)
@@ -11,11 +13,20 @@ const lifx = (selector, method, action, params) =>
       .then(console.log)
       .catch(console.error)
 
+const skull_effect = () =>
+      request['post'](`${skullApiUrl}`, {event_body: {password: skullApiPassword}})
+        .send()
+        .then(console.log)
+        .catch(console.error)
+
 charge.stream().on('payment', async inv => {
   console.log('inv', inv)
   if (inv.metadata && inv.metadata.lightColor) {
     lifx('all', 'post', 'effects/breathe', { color: `${inv.metadata.lightColor} brightness:0.8`, cycles: 1, period: 10, peak: 0.05 })
   } else {
+    if (inv.metadata && inv.metadata.skullEffect) {
+      skull_effect()
+    }
     lifx('id:d073d5247913', 'post', 'effects/pulse', { color: 'blue brightness:1', cycles: 3, period: 0.1 })
     setTimeout(_ => lifx('id:d073d524e344', 'post', 'effects/pulse', { color: 'blue brightness:1', cycles: 3, period: 0.1 }), 500)
   }
